46a491c4b1506cfe2dd1456f8c7f0b0660c2b980
Ryan Lim <rlim@bsd220.org>
2022-02-15 10:29:49 -0600

    Added Element Custom Struct

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -2,12 +2,13 @@
 //  ContentView.swift
 //  API Calling
 //
-//  Created by Student on 2/15/22.
+//  Created by Ryan Lim on 2/15/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var elements = [Element]()
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -18,4 +19,14 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Element: Identifiable {
+    let id = UUID()
+    var atomicMass: String
+    var atomicNumber: String
+    var meltingPoint: String
+    var boilingPoint: String
+    var symbol: String
+    var groupBlock: String
 }
